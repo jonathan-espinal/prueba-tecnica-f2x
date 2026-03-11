17cc00c95e85340830bc0836fcfa5d687b4dd4e7
Jonathan Espinal <jonatha.espinal@gmail.com>
2026-03-11 18:26:45 -0500

    First upload

---
+++ src/main/java/com/f2x/prueba/PruebaApplication.java
@@ -0,0 +1,13 @@
+package com.f2x.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApplication.class, args);
+	}
+
+}
